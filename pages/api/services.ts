e091d8eb5e307a737fa66faa5c5bc7fb44cb93f4
luca <lucabonasera2@gmail.com>
2022-01-04 15:56:38 +0100

    remove warning in service.ts

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { services } from "../../data";
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (req:NextApiRequest, res:NextApiResponse) => {
 
     res.status(200).json({services});
